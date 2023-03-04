ab4fc4b3bc29ffc293b8ab9ba48beac9fb36f009
fernandohugosoftwaredeveloper <fernandohugosoftwaredeveloper@gmail.com>
2023-03-04 19:28:13 -0300

    open-api-spring

--- car-rental/src/main/java/com/bee/carrental/api/CarRentalApplication.java
+++ car-rental/src/main/java/com/bee/carrental/api/CarRentalApplication.java
@@ -1,9 +1,11 @@
 package com.bee.carrental.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class CarRentalApplication {
 
 	public static void main(String[] args) {
